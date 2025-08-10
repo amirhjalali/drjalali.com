25ae3a44f7531c41401379d697a8bb72fd4ccee4
Amir Jalali <amirhjalali@gmail.com>
2025-08-10 12:00:13 -0400

    Update GPT5Todo.md with completed tasks and add react-icons for social links

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { Send, User, Mail, MessageSquare } from 'lucide-react';
+import { SiGooglescholar, SiLinkedin, SiIeee, SiYoutube, SiInstagram, SiWikipedia } from 'react-icons/si';
 
 const ContactForm = () => {
   const [contactFormData, setContactFormData] = useState({
@@ -64,65 +65,38 @@ const ContactForm = () => {
     {
       name: 'Google Scholar',
       href: 'https://scholar.google.com/citations?user=yb5J4skAAAAJ&hl=en',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/>
-        </svg>
-      ),
-      color: 'hover:text-blue-600'
+      icon: <SiGooglescholar className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-[#4285F4]'
     },
     {
       name: 'LinkedIn',
       href: 'https://www.linkedin.com/in/aliakbar-jalali-4569405/',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-        </svg>
-      ),
-      color: 'hover:text-blue-600'
+      icon: <SiLinkedin className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-[#0A66C2]'
     },
     {
       name: 'IEEE Xplore',
       href: 'https://ieeexplore.ieee.org/author/37300412300',
-      icon: (
-        <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24">
-          {/* IEEE Official Logo Style */}
-          <path d="M2 2h20v20H2V2zm18 18V4H4v16h16zM6 6h4v1.5H6V6zm0 2.5h4V10H6V8.5zM6 11h4v1.5H6V11zm6-5h4v1.5h-4V6zm0 2.5h4V10h-4V8.5zm0 2.5h4v1.5h-4V11zM6 13.5h4V15H6v-1.5zm6 0h4V15h-4v-1.5zM6 16h10v1.5H6V16z" fill="currentColor"/>
-          <circle cx="18" cy="6" r="1.5" fill="currentColor"/>
-        </svg>
-      ),
-      color: 'hover:text-blue-700'
+      icon: <SiIeee className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-[#00629B]'
     },
     {
       name: 'YouTube',
       href: 'https://www.youtube.com/@Professor.Aliakbar.Jalali',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-        </svg>
-      ),
-      color: 'hover:text-red-600'
+      icon: <SiYoutube className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-[#FF0000]'
     },
     {
       name: 'Instagram',
       href: 'https://www.instagram.com/drjalali_ict/?hl=en',
-      icon: (
-        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-        </svg>
-      ),
-      color: 'hover:text-pink-500'
+      icon: <SiInstagram className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-[#E4405F]'
     },
     {
       name: 'Wikipedia',
       href: 'https://en.wikipedia.org/wiki/Ali_Akbar_Jalali',
-      icon: (
-        <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24">
-          {/* Wikipedia W Logo - Official Style */}
-          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.5 8.5h1.8l1.2 6L12 10l1.5 4.5 1.2-6h1.8L15 18h-1.5L12 12l-1.5 6H9L7.5 8.5z" fill="currentColor"/>
-        </svg>
-      ),
-      color: 'hover:text-gray-700'
+      icon: <SiWikipedia className="w-5 h-5" aria-hidden="true" />,
+      color: 'hover:text-gray-800 dark:hover:text-gray-200'
     }
   ];
 
